c7ade1ec5a4d91ed72b2ae88f7409c8d10ede0a2
ra870860 <rafarealeisv9@gmail.com>
2025-10-27 20:10:49 +0100

    Completado hasta apartado 2 punto 1

---
+++ Practica_Python_RafelNuñezPedrosa.py
@@ -0,0 +1,11 @@
+nombre_empresa="TechSolutions"
+año_fundacion=2010
+print("La empresa", nombre_empresa, "fue fundada en el año", año_fundacion)
+
+numero_usuario=int(input("Introduce un número: "))
+if numero_usuario == 0:
+    print("El número es cero.")
+elif numero_usuario > 0:
+    print("El número es positivo.")
+else:
+    print("El número es negativo.")
